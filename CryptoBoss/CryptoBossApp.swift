51d374780f62bd99cf8479c004c0d7c20fcfb6cf
Mariusz Zając <zajac@gmx.us>
2022-11-03 08:18:18 +0100

    Initial Commit

---
+++ CryptoBoss/CryptoBossApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoBossApp.swift
+//  CryptoBoss
+//
+//  Created by Mariusz Zając on 03/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoBossApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
